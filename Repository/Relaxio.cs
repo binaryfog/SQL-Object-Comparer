8a223461eb56eb115d22bfec3aca55ec01b926b4
Catalin Hatmanu <harapu@hotmail.com>
2018-04-28 21:13:39 -0400

    First serious one

---
+++ Repository/Relaxio.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Repository
+{
+    public class Relaxio
+    {
+        public string Name { get; set; }
+
+    }
+}
---
+++ SQL Object Comparer/Controllers/SampleDataController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AngularSPABootstrap4.Controllers
+{
+    [Route("api/[controller]")]
+    public class SampleDataController : Controller
+    {
+        private static string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        [HttpGet("[action]")]
+        public IEnumerable<WeatherForecast> WeatherForecasts()
+        {
+            Repository.Relaxio r = new Repository.Relaxio();
+            var rng = new Random();
+            r.Name = Summaries[rng.Next(Summaries.Length)];
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                DateFormatted = DateTime.Now.AddDays(index).ToString("d"),
+                TemperatureC = rng.Next(-20, 55),
+                //Summary = Summaries[rng.Next(Summaries.Length)]
+                Summary = r.Name
+                
+            });
+        }
+
+        public class WeatherForecast
+        {
+            public string DateFormatted { get; set; }
+            public int TemperatureC { get; set; }
+            public string Summary { get; set; }
+
+            public int TemperatureF
+            {
+                get
+                {
+                    return 32 + (int)(TemperatureC / 0.5556);
+                }
+            }
+        }
+    }
+}
